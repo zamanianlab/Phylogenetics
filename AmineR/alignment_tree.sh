f5f31ed0306cb60b4b62eea04e8f1d26b4541db5
Nic Wheeler <nicolas.j.wheeler@gmail.com>
2020-06-29 13:45:16 -0500

    Tree building on server

--- AmineR/alignment_tree.sh
+++ AmineR/alignment_tree.sh
@@ -60,4 +60,20 @@ HMMTOP_strict_py="$gh_dir/aux/scripts/HMMTOP_extract_strict.py"
 
 ## align (see https://mafft.cbrc.jp/alignment/software/algorithms/algorithms.html)
 ## for algorithm description
-einsi --reorder --thread 8 $out/3/all_aminer_tm.fa > $out/3/all_aminer_tm_1.aln
+# einsi --reorder --thread 8 $out/3/all_aminer_tm.fa > $out/3/all_aminer_tm_1.aln
+
+## trim and filter
+# trimal -gt 0.7 -in $out/3/all_aminer_tm_1.aln -out $out/3/all_aminer_tm_2.aln
+# trimal -resoverlap 0.70 -seqoverlap 70 -in $out/3/all_aminer_tm_2.aln -out $out/3/all_aminer_tm_3.aln
+
+## change to single-line FASTA
+# awk -f $gh_dir/aux/scripts/linearizefasta.awk < $out/3/all_aminer_tm_2.aln > $out/3/all_aminer_tm_2_linear.aln
+# awk -f $gh_dir/aux/scripts/linearizefasta.awk < $out/3/all_aminer_tm_3.aln > $out/3/all_aminer_tm_3_linear.aln
+
+## Get IDs and compare lists
+# cat $out/3/all_aminer_tm_2_linear.aln | tr '\t' '\n' | awk 'NR%2==1' > $out/3/all_aminer_tm_2_ids.txt
+# cat $out/3/all_aminer_tm_3_linear.aln | tr '\t' '\n' | awk 'NR%2==1' > $out/3/all_aminer_tm_3_ids.txt
+# grep -v -f $out/3/all_aminer_tm_3_ids.txt $out/3/all_aminer_tm_2_ids.txt > $out/3/all_aminer_3_filtered.txt
+
+## ML tree on server
+iqtree -s $out/3/all_aminer_tm_3.aln -nt 4 -alrt 1000 -bb 1000
