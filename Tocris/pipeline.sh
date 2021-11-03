a6e7e0b0fceacc0f08721e1cff93f7a5ac767749
Katie Ryan <ktryan2@wisc.edu>
2021-11-03 15:10:10 -0500

    added parasite_db file; made 3 and 4 directories (in output); added parasite blast sections

--- Tocris/pipeline.sh
+++ Tocris/pipeline.sh
@@ -40,14 +40,19 @@ rm $proteomes/*.protein*.gz
 
 makeblastdb -in $proteomes/HsUniProt_nr.fasta -dbtype prot
 
-
+# set up directories and move files
 mv Phylogenetics/Tocris/Hs_seeds.list.txt work
 mkdir work/1_Hs_seeds
 seeds=work/1_Hs_seeds
-mkdir work/Hs_targets
-Hs_targets=work/Hs_targets
+mkdir work/2_Hs_targets
+Hs_targets=work/2_Hs_targets
 mkdir output/alignments
-mv Phylogenetics/Tocris/parasite.list.txt work
+alignments=output/alignments
+mv Phylogenetics/Tocris/parasite_db.list.txt work
+mkdir output/3_Para_targets
+Para_targets=output/3_Para_targets
+mkdir output/Para_final
+Para_final=output/Parafinal
 
 # Get IDs and sequences of hits
 while IFS= read -r line; do
@@ -63,15 +68,15 @@ while IFS= read -r line; do
 	seqtk subseq $proteomes/HsUniProt_nr.fasta $Hs_targets/"$line_sub".list.txt > $Hs_targets/"$line_sub".ext.fasta
   rm $Hs_targets/*.out
 
-	# cat work/Hs_targets/$line_sub.ext.fasta | sed 's/>/>Homo_sapiens|/g' > output/alignments/$line_sub.combined.fasta
-	# while IFS= read -r paradb; do
-	# 	#blast expanded human targets against parasite dbs
-	# 	para_name=$(echo "$paradb" | awk 'BEGIN { FS = "." } ; { print $1 }')
-	# 	echo "$para_name"
-	# 	blastp -query Hs_targets/$line_sub.ext.fasta -db parasite_db/$paradb -out Para_blast/$line_sub.$para_name.out -outfmt 6 -max_hsps 1 -evalue 1E-1 -num_threads 4
-	# 	cat Para_blast/$line_sub.$para_name.out | awk '$3>30.000 && $11<1E-3 {print $2}' | sort | uniq  > Para_blast/$line_sub.$para_name.list.txt
-	# 	seqtk subseq parasite_db/$paradb Para_blast/$line_sub.$para_name.list.txt >  Para_targets_1/$line_sub.$para_name.fasta
-  # done < parasite_db/parasite_db.list.txt
+	cat $Hs_targets/"$line_sub".ext.fasta | sed 's/>/>Homo_sapiens|/g' > $alignments/"$line_sub".combined.fasta
+	while IFS= read -r paradb; do
+		#blast expanded human targets against parasite dbs
+		para_name=$(echo "$paradb" | awk 'BEGIN { FS = "/" } ; { print $1 }')
+		echo "$para_name"
+		blastp -query $Hs_targets/"$line_sub".ext.fasta -db $proteomes/"$paradb" -out $Para_targets/"$line_sub"."$para_name".out -outfmt 6 -max_hsps 1 -evalue 1E-1 -num_threads 4
+		cat $Para_targets/"$line_sub"."$para_name".out | awk '$3>30.000 && $11<1E-3 {print $2}' | sort | uniq  > $Para_targets/"$line_sub"."$para_name".list.txt
+		seqtk subseq $proteomes/$paradb $Para_targets/"$line_sub"."$para_name".list.txt >  $Para_final/"$line_sub"."$para_name".fasta
+  done < work/parasite_db.list.txt
 done < work/Hs_seeds.list.txt
 # 		#blast parasite hits against human db
 # 		blastp -query Para_targets_1/$line_sub.$para_name.fasta -db human_db/HsUniProt_nr.fasta -out Recip_blast/$line_sub.$para_name.out -outfmt 6 -max_hsps 1 -evalue 1E-3 -num_threads 4
