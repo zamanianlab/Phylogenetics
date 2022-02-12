7ca42a990d48eafbbe5f836491293bd2c9e226ff
Katie Ryan <ktryan2@wisc.edu>
2022-02-12 14:07:34 -0600

    change iqtree to auto cpu delegation

--- Tocris/pipeline.sh
+++ Tocris/pipeline.sh
@@ -84,7 +84,7 @@ while IFS= read -r paradb; do
     trimal -gt 0.7 -in $alignments/"$line_sub".combined.aln -out $alignments/"$line_sub".combined_trim.aln
     trimal -resoverlap 0.70 -seqoverlap 70 -in $alignments/"$line_sub".combined_trim.aln -out $alignments/"$line_sub".combined_final.aln
     #tree-building
-    iqtree-2.1.3-MacOSX/bin/iqtree2 -s $alignments/"$line_sub".combined_final.aln -nt 10 -alrt 1000 -bb 1000
+    iqtree-2.1.3-MacOSX/bin/iqtree2 -s $alignments/"$line_sub".combined_final.aln -nt AUTO -alrt 1000 -bb 1000
 done < Phylogenetics/Tocris/parasite_db.list.txt
 
 mv $alignments output
